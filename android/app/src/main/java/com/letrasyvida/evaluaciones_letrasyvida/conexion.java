047721579cd1375be4a12348c225e50b450e5211
Edgar Laz <edgarguitarist@gmail.com>
2022-01-13 22:58:01 -0500

    This directory don't belong here.
    Now it's an other repository

--- android/app/src/main/java/com/letrasyvida/evaluaciones_letrasyvida/conexion.java
+++
@@ -1,6 +0,0 @@
-package com.letrasyvida.evaluaciones_letrasyvida;
-
-public class conexion {
-    public static final String URL_WEB_SERVICES ="https://aulavirtual.letrasyvida.com/apk/";
-    public static final String TAG_USERNAME = "username";
-}
